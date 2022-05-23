98f88d47bcbfc241f167250cb086ec5bd06251c7
charlesretro <105062799+charlesretro@users.noreply.github.com>
2022-05-23 16:08:16 -0300

    Add files via upload

---
+++ assets/js/home.js
@@ -0,0 +1,25 @@
+var _i = 0;
+
+function homeRanking(i) {
+    _i += i;
+    _i = _i < 0 ? 0 : _i;
+
+    $.ajax({
+        url: "#", type: "post", data: {start: _i}, success: function (e) {
+            $("#cnRankingH").html(e);
+        }
+    });
+}
+
+$(document).ready(function () {
+    //homeRanking(0);
+});
+
+function homeNoticias(i) {
+
+    $.ajax({
+        url: "#", type: "post", data: {start: i}, success: function (e) {
+            $("#areaNoticias").html(e);
+        }
+    });
+}
---
+++ assets/js/script.js
@@ -0,0 +1,137 @@
+const x = window.matchMedia("(max-width: 1070px)");
+const mobile = window.matchMedia("(max-width: 750px)");
+const newsSwiper = window.matchMedia("(max-width: 1068px)");
+const menuMobile = document.querySelector(".menuMobile");
+const menu = document.querySelector(".menu");
+const panel = document.querySelector(".panel");
+const logo = document.querySelector(".logo img");
+const social = document.querySelector(".social");
+const flags = document.querySelector(".langIcons");
+const news = document.querySelector("main");
+const mySwiper = document.querySelector(".mySwiper");
+const imgNaruto = document.querySelector(".contentImg");
+const a1 = document.querySelector(".a1");
+const b1 = document.querySelector(".b1");
+const c1 = document.querySelector(".c1");
+const c2 = document.querySelector(".c2");
+const circles = document.querySelectorAll('[data-js="c"]');
+const author = document.querySelectorAll(".author");
+const boxImg = document.querySelector(".boxIMG img");
+const boxTitle = document.querySelector(".boxTitle h3");
+
+function activeCircle(e) {
+    circles.forEach((c) => {
+        if (c.classList.contains("circleActive")) {
+            c.classList.remove("circleActive");
+            console.log(this);
+        }
+    });
+    this.classList.add("circleActive");
+
+    const title = this.getAttribute("title");
+    const cargo = this.getAttribute("cargo");
+    const img = this.getAttribute("data-img");
+    const titleBig = this.getAttribute("boxTitle");
+    boxImg.src = img;
+    console.log(boxImg.src)
+    boxTitle.innerText = titleBig;
+
+    if (titleBig === "KARIN") {
+        boxTitle.style.color = "#8d3132";
+    } else if (titleBig === "ROCK LEE") {
+        boxTitle.style.color = "#8257e6";
+    } else if (titleBig === "KIMIMARU") {
+        boxTitle.style.color = "#fc8834";
+    } else {
+        boxTitle.style.color = "#5b8c79";
+    }
+
+    const titleH2 = document.createElement("h2");
+    const cargoP = document.createElement("p");
+    const p = (cargoP.innerText = cargo);
+    const h2 = (titleH2.innerText = title);
+
+    document.querySelector(".author h3").innerHTML = h2.toUpperCase();
+    document.querySelector(".author p").innerHTML = p;
+
+    console.log(document.querySelector(".author h3"));
+}
+
+circles.forEach((c) => c.addEventListener("click", activeCircle));
+if (a1 && b1) {
+
+    a1.classList.add("showLeft");
+
+    function showImage() {
+        if (a1.classList.contains("showLeft")) {
+            a1.classList.remove("showLeft");
+            b1.classList.add("showLeft");
+        } else {
+            a1.classList.add("showLeft");
+            b1.classList.remove("showLeft");
+        }
+
+    }
+
+    setInterval(showImage, 5000);
+}
+if (c1 && c2) {
+
+    c1.classList.add("showLeft");
+
+    function showImage() {
+        if (c1.classList.contains("showLeft")) {
+            c1.classList.remove("showLeft");
+            c2.classList.add("showLeft");
+        } else {
+            c1.classList.add("showLeft");
+            c2.classList.remove("showLeft");
+        }
+    }
+
+    setInterval(showImage, 5000);
+}
+
+function test(x) {
+    if (x.matches) {
+        menuMobile.style.display = "inline-block";
+        menu.style.display = "none";
+    } else {
+        menuMobile.style.display = "none";
+        menu.style.display = "flex";
+    }
+
+    if (mobile.matches) {
+        social.style.display = "none";
+        flags.style.display = "none";
+        logo.src = "assets/images/logo-mobile.png";
+    } else {
+        logo.src = "assets/images/logo.png";
+        social.style.display = "flex";
+        flags.style.display = "flex";
+    }
+}
+
+function aaa() {
+    if (panel.classList.contains("show")) {
+        panel.classList.remove("show");
+        menuMobile.classList.remove("round");
+    } else {
+        menuMobile.classList.add("round");
+        panel.classList.add("show");
+    }
+}
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto", spaceBetween: 3, pagination: {
+        el: ".swiper-pagination", clickable: true,
+    },
+});
+
+menuMobile.addEventListener("click", aaa);
+x.addEventListener("change", test);
+mobile.addEventListener("change", test);
+test(x);
+test(mobile);
+test(newsSwiper);
+AOS.init();
